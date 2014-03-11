e1c6881a794dc9cedc23aafa4fd472ed1b751e4b
Chris Wagner <cswagnr@gmail.com>
2014-03-11 13:12:45 -0400

    Direct image manipulation
    Rather than using the rectangle method in Matlab to draw rectangles,
    stippling is now done through direct pixel value manipulation.
    
    Working towards drawing other shapes besides rectangles.

--- scripts/jlaw_test.m
+++ scripts/jlaw_test.m
@@ -4,9 +4,8 @@
 %   reads in a test image, converts it to grayscale, stipples the image,
 %   saves the stipple image in a new .jpg, closes the image figure
 %
-%   Chris Wagner, 2.13.14
+%   Chris Wagner, 3.11.14
 %----------------------------------------------------------------------
 img = rgb2gray(im2double(imread('../images/jlaw.jpg')));
-[pixelated, pat] = stipple(img, 12, 12);
-print(gcf, '-djpeg', '../output/jlaw-stipple.jpg');
-close all;
+[outImg, pat] = stipple(img, 12, 12);
+imwrite(outImg, '../output/jlaw-stipple.jpg');
